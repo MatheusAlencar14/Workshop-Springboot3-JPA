732d38c424caa7786376f0ca43019ea91e762308
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-09-19 11:51:52 -0300

    Implementando entidade Category

---
+++ src/main/java/com/devjava/cursojava/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.devjava.cursojava.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devjava.cursojava.entities.Category;
+import com.devjava.cursojava.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
