08ba999ef51a1d9babe7131383a26bc0a3af04cb
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-09-20 15:08:28 -0300

    Entidade Payment(Pagamento) e Associação 1*1

--- src/main/java/com/devjava/cursojava/config/TestConfig.java
+++ src/main/java/com/devjava/cursojava/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.devjava.cursojava.entities.Category;
 import com.devjava.cursojava.entities.Order;
 import com.devjava.cursojava.entities.OrderItem;
+import com.devjava.cursojava.entities.Payment;
 import com.devjava.cursojava.entities.Product;
 import com.devjava.cursojava.entities.User;
 import com.devjava.cursojava.entities.enums.OrderStatus;
@@ -81,5 +82,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-07-22T17:21:22Z"), o3);
+		o3.setPayment(pay1);
+		
+		orderRepository.save(o3);
 	}
 }
