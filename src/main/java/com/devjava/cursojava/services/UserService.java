53dcda7415f0ccafcabd6700f6e8f648f8a68c26
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-09-20 16:20:45 -0300

    Implementando Atualização (método Update) de User

--- src/main/java/com/devjava/cursojava/services/UserService.java
+++ src/main/java/com/devjava/cursojava/services/UserService.java
@@ -31,4 +31,18 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User user) {
+		User entity = repository.getReferenceById(id);
+		UpdateData(entity, user);
+		return repository.save(entity);
+	}
+
+	private void UpdateData(User entity, User user) {
+		 entity.setName(user.getName());
+		 entity.setEmail(user.getEmail());
+		 entity.setPhone(user.getPhone());
+	}
+	
+	
 }
