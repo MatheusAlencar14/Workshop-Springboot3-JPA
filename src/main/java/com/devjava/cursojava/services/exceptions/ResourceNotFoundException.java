21049d18f59867806d3bff15b87c0b1308eb5b15
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-09-21 11:10:47 -0300

    Tratamento de exceção - findById

---
+++ src/main/java/com/devjava/cursojava/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.devjava.cursojava.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id: " + id);
+	}
+}
